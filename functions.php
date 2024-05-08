6f889353032c3f8058d31d273330828e32efa7d2
Samiul H Pranto <samihoq10@gmail.com>
2024-05-08 17:45:45 +0600

    new code for blogX theme

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php 
+
+add_theme_support( 'menus' );
+add_theme_support( 'widgets' );
+add_theme_support( 'custom-header' );
+add_theme_support( 'custom-background' );
+add_theme_support( 'post-formats',['video','audio','gallery'] );
+add_theme_support( 'post-thumbnails');
+add_theme_support( 'custom-logo' );
+add_theme_support( 'title-tag' );
+add_theme_support( 'title-tag' );
+
+?>
---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang=" <?php language_attributes() ?> ">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Theme Development</title>
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+
+
+</head>
+<body>
+    
+
+<section class="menu_section">
+    <div class="menu">
+        <div class="mid">
+            <div class="logo">
+                <img src=" <?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="">
+            </div>
+            <ul>
+                <li><a href="#"></a>Home</li>
+                <li><a href="#"></a>About</li>
+                <li><a href="#"></a>Blog</li>
+                <li><a href="#"></a>Info</li>
+                <li><a href="#"></a>Contact</li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+
+<section class="header_image">
+    <img src=" <?php echo get_template_directory_uri(  ); ?>/images/using laptop.jpg " alt="">
+</section>
+
+
+
+<section class="posts">
+<div class="post">
+    <h2>Latest Posts</h2>
+    <hr>
+
+    <div class="post_mid">
+
+        <div class="post1">
+            <img src="" alt="">
+            <h3>Post No. 01</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias quisquam quis, necessitatibus alias earum, cumque soluta hic debitis neque optio voluptatibus doloribus placeat asperiores distinctio consequuntur, commodi itaque nesciunt? Deserunt ullam odio excepturi hic magni incidunt atque eligendi perspiciatis dolores velit, quasi est, dolorum tenetur exercitationem repudiandae unde. Quia eius voluptate quisquam esse id blanditiis eos delectus magni reiciendis. Voluptates dolor quis mollitia laboriosam fuga vero delectus accusamus consequatur! Qui in earum voluptatem facere accusantium dolore magni. Vero aperiam, assumenda repudiandae molestias nesciunt maiores odit deleniti eveniet omnis ducimus quis officia placeat non maxime ea architecto libero magnam pariatur ipsum distinctio? Quas explicabo sint itaque laboriosam doloremque fugit voluptas ad temporibus laudantium aliquam iusto aliquid asperiores laborum sequi perspiciatis, numquam libero sed eveniet. Delectus facere temporibus labore, quibusdam asperiores quas repellat debitis facilis quidem totam vel saepe nulla officia doloremque deleniti adipisci placeat sint ratione consequatur quasi recusandae a?</p>
+        </div>
+
+        <div class="post1">
+            <img src="" alt="">
+            <h3>Post No. 02</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias quisquam quis, necessitatibus alias earum, cumque soluta hic debitis neque optio voluptatibus doloribus placeat asperiores distinctio consequuntur, commodi itaque nesciunt? Deserunt ullam odio excepturi hic magni incidunt atque eligendi perspiciatis dolores velit, quasi est, dolorum tenetur exercitationem repudiandae unde. Quia eius voluptate quisquam esse id blanditiis eos delectus magni reiciendis. Voluptates dolor quis mollitia laboriosam fuga vero delectus accusamus consequatur! Qui in earum voluptatem facere accusantium dolore magni. Vero aperiam, assumenda repudiandae molestias nesciunt maiores odit deleniti eveniet omnis ducimus quis officia placeat non maxime ea architecto libero magnam pariatur ipsum distinctio? Quas explicabo sint itaque laboriosam doloremque fugit voluptas ad temporibus laudantium aliquam iusto aliquid asperiores laborum sequi perspiciatis, numquam libero sed eveniet. Delectus facere temporibus labore, quibusdam asperiores quas repellat debitis facilis quidem totam vel saepe nulla officia doloremque deleniti adipisci placeat sint ratione consequatur quasi recusandae a?</p>
+        </div>
+
+    </div>
+</div>
+</section>
+
+</body>
+</html>
